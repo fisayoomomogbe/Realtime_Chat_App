b3c56d372683893c8f839541ac40ade12392fc65
fisayoomomogbe <doctoromo@yahoo.com>
2021-05-10 21:41:26 -0600

    updated app.js with necessary functions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -9,17 +10,26 @@ const io = socketio(server);
 // Middlewares
 app.use(express.static(path.join(__dirname, "public")));
 
+const adminName = "ChatTime Admin";
+
 // Run when a client connects
 io.on("connection", (socket) => {
   //   Welcome current user
-  socket.emit("message", "Welcome to ChatTime");
+  socket.emit("message", formatMessage(adminName, "Welcome to ChatTime!"));
 
   //   broadcast when a user connects
-  socket.broadcast.emit("message", "A user has joined the chat");
+  socket.broadcast.emit(
+    "message",
+    formatMessage(adminName, "A user has joined the chat")
+  );
 
   // Runs when client disconnects
   socket.on("disconnect", () => {
-    io.emit("message", " A user has left the chat");
+    io.emit("message", formatMessage(adminName, " A user has left the chat"));
+  });
+  //  Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", formatMessage("USER", msg));
   });
 });
 
