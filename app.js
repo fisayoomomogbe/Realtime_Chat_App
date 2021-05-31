cc3acc3c3b174ef314005783a045714cb140fc2c
fisayoomomogbe <doctoromo@yahoo.com>
2021-05-31 17:20:53 -0600

    Updated app.js

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ io.on("connection", (socket) => {
     }
   });
 });
-server.listen("4000", () => {
+server.listen(process.env.PORT || 4000, () => {
   console.log("Server is running on port 4000");
 });
